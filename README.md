afba027040b069b36de5f614add74bcb044a371c
SarykL <saryk.lav@gmail.com>
2015-09-18 00:18:14 +0300

    fc

---
+++ README.md
@@ -0,0 +1,4 @@
+QSM
+Boilerplate for quick start markup
+
+After generating "sprite.scss", change the path in "@mixin sprite-image($sprite) "
---
+++ gulpfile.js
@@ -0,0 +1,129 @@
+'use strict';
+
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    prefixer = require('gulp-autoprefixer'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rigger = require('gulp-rigger'),
+    cssmin = require('gulp-minify-css'),
+    imagemin = require('gulp-imagemin'),
+    spritesmith = require('gulp.spritesmith'),
+    rimraf = require('rimraf'),
+    pngquant = require('imagemin-pngquant'),
+    browserSync = require("browser-sync"),
+    reload = browserSync.reload;
+
+var path = {
+    build: {
+        html: 'app/',
+        css: 'app/css/',
+        img: 'app/img/',
+        fonts: 'app/fonts/'
+    },
+    src: {
+        html: 'src/*.html',
+        style: 'src/style/main.scss',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    watch: {
+        html: 'src/**/*.html',
+        style: 'src/style/**/*.scss',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    clean: './app'
+};
+
+gulp.task('html:build', function () {
+    gulp.src(path.src.html) 
+        .pipe(rigger())
+        .pipe(gulp.dest(path.build.html))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('style:build', function () {
+    gulp.src(path.src.style) 
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            includePaths: ['src/style/'],
+            outputStyle: 'compressed',
+            sourceMap: true,
+            errLogToConsole: true
+        }))
+        .pipe(prefixer())
+        .pipe(cssmin())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.build.css))
+        .pipe(reload({stream: true}));
+});
+
+var config = {
+    server: {
+        baseDir: "./app"
+    },
+    tunnel: true,
+    host: 'localhost',
+    port: 9000,
+    logPrefix: "Quick-Start"
+};
+
+gulp.task('webserver', function () {
+    browserSync(config);
+});
+
+gulp.task('sprite', function () {
+  var spriteData = gulp.src('src/sprites/*.png').pipe(spritesmith({
+    imgName: 'sprite.png',
+    cssName: 'sprite.scss'
+  }));
+  return spriteData.pipe(gulp.dest('app/sprite'));
+});
+
+gulp.task('image:build', function () {
+    gulp.src(path.src.img) 
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()],
+            interlaced: true
+        }))
+        .pipe(gulp.dest(path.build.img))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('fonts:build', function() {
+    gulp.src(path.src.fonts)
+        .pipe(gulp.dest(path.build.fonts))
+});
+
+gulp.task('build', [
+    'html:build',
+    'style:build',
+    'fonts:build',
+    'image:build'
+]);
+
+gulp.task('clean', function (cb) {
+    rimraf(path.clean, cb);
+});
+
+
+gulp.task('watch', function(){
+    watch([path.watch.html], function(event, cb) {
+        gulp.start('html:build');
+    });
+    watch([path.watch.style], function(event, cb) {
+        gulp.start('style:build');
+    });
+    watch([path.watch.img], function(event, cb) {
+        gulp.start('image:build');
+    });
+    watch([path.watch.fonts], function(event, cb) {
+        gulp.start('fonts:build');
+    });
+});
+
+
+gulp.task('default', ['build', 'watch']);
